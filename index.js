8bbbef3f694137b5255a4d54fb5eba94a46226e6
majjeed <abdulmajeed.hersi@gmail.com>
2024-06-13 13:49:21 +0100

    passing capitalize and reverseString tests

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function sum(a, b) {
   return a + b;
 }
-export { sum };
+
+function capitalize(str) {
+  return str.slice(0, 1).toUpperCase() + str.slice(1);
+}
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+export { sum, capitalize, reverseString };
--- index.test.js
+++ index.test.js
@@ -1,6 +1,14 @@
 import { test, expect } from "@jest/globals";
-import { sum } from "./index";
+import { sum, capitalize, reverseString } from "./index";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test("capitalize the first character", () => {
+  expect(capitalize("hello")).toBe("Hello");
+});
+
+test("reverses a string", () => {
+  expect(reverseString("hello")).toMatch("olleh");
 });
